b66f4b3272775f9bd7a450dc137e8240f8259362
Soubhik <soubhik960@gmail.com>
2024-04-30 14:19:00 +0530

    add config

--- aqua_modules/router/initialise.php
+++ aqua_modules/router/initialise.php
@@ -1,4 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-require_once import("router");
+class AppRouter {
+    
+}
---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+
+define("BASEPATH", __DIR__);
+$pass_url = 'CPHP';
+$localhost = $_SERVER["SERVER_NAME"];
+define("BASEURL", "http://$localhost/$pass_url/");
+$request = $_SERVER['REQUEST_URI'];
+
+// Specify the file path
+$load_config = "config.json";
+$load_config_lock = "config-lock.json";
+$config = [];
+$config_lock = [];
+
+// Check if the file exists
+if (file_exists($load_config)) {
+    $config = json_decode(file_get_contents($load_config));
+} else {
+    // If the file doesn't exist, display an error message
+    echo "File not found!";
+}
+
+if (file_exists($load_config_lock)) {
+    $config_lock = json_decode(file_get_contents($load_config_lock));
+} else {
+    // If the file doesn't exist, display an error message
+    echo "File not found!";
+}
+
+echo "<pre>";
+// print_r($config);
+// print_r($config_lock);
+echo "</pre>";
+
+function url($a_url){
+    return BASEURL.$a_url;
+}
+
+function import($path){
+    // return BASEPATH.$config_lock->packages->core->path;
+    global $config_lock;
+    try {
+        eval('$r = BASEPATH.$config_lock->packages->'.$path.'->path;');
+        return $r;
+    } catch (\Throwable $th) {
+        return 'error';
+    }
+}
+
+require_once import("core");
