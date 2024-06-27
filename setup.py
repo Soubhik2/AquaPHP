a4986126d94a67a1a44795fd9a83ca32fe9cd981
Soubhik <soubhik960@gmail.com>
2024-06-27 19:37:50 +0530

    add intstall function

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ import subprocess
 import time
 import json
 import requests
+import shutil
 
 # ANSI escape codes for text colors
 color_red = '\033[91m'
@@ -22,72 +23,101 @@ def init():
     arr = current_directory.split("\\")
 
     path = '/'.join(arr[arr.index("htdocs")+1:])
-    path += '/'+name
+    # path += '/'+name
 
     # Open the file for reading
-    with open(name+'/index.php', 'r') as file:
+    with open('index.php', 'r') as file:
         # Read the entire contents of the file
         file_contents = file.read()
         # Print the contents
 
-    with open(name+'/index.php', 'w') as f:
-        f.write(file_contents.replace("CPHP", path))
+    with open('index.php', 'w') as f:
+        f.write(file_contents.replace("AquaPHP", path))
 
-    with open(name+'/.htaccess', 'r') as file:
+    with open('.htaccess', 'r') as file:
         # Read the entire contents of the file
         file_contents_h = file.read()
         # Print the contents
 
-    with open(name+'/.htaccess', 'w') as f:
-        f.write(file_contents_h.replace("CPHP", path))
+    with open('.htaccess', 'w') as f:
+        f.write(file_contents_h.replace("AquaPHP", path))
 
     print(color_green+"\nProject Initiated"+color_reset)
 
+def download_package(package):
+    # print(package)
+    print(color_blue+"downloading packages...\n")
+    process = subprocess.Popen(["git", "clone", package["url"], package['path']], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    while True:
+        line = process.stdout.readline()
+        if not line:
+            break
+        print(line.strip())
+        line = process.stderr.readline()
+        if not line:
+            break
+        print(line.strip())
+
+    process.wait()
+
+    if process.returncode == 0:
+        print(color_green+"package downloaded successful\n"+color_reset)
+        return True
+        # output = subprocess.run(["rm","-rf",name+"/",".git"], shell=True, capture_output=True, text=True)
+        # output = subprocess.run(["Remove-Item","-Recurse","-Force",name+"\\",".git"], shell=True, capture_output=True, text=True)
+    else:
+        print(color_red+"package download failed\n"+color_reset)
+        return False
+
 def set_package(data, command):
-    print(color_blue+"installing packages...\n")
-    
-    # print(data["package"])
-    
-    time.sleep(1)
 
-    # print(color_purple+"Install Starting..."+color_reset)
-    # time.sleep(1)
+    if download_package(data["package"]):
+        print(color_blue+"installing packages...\n")
+        
+        # print(data["package"])
+        
+        time.sleep(1)
 
-    with open('config.json', 'r') as file:
-        # Read the entire contents of the file
-        file_contents = file.read()
-        file_contents = json.loads(file_contents)
-        # file_contents["dependencies"][command] = {
-        #     "enable": False,
-        #     "path": "auth"
-        # }
-        file_contents["dependencies"][command] = data["config"][command]
-        file_contents = json.dumps(file_contents, indent=4)
-        # print(file_contents)
-        # Print the contents
+        # print(color_purple+"Install Starting..."+color_reset)
+        # time.sleep(1)
 
-    with open('config.json', 'w') as f:
-        f.write(file_contents)
-    
-    with open('config-lock.json', 'r') as file:
-        # Read the entire contents of the file
-        file_contents = file.read()
-        file_contents = json.loads(file_contents)
-        # file_contents["packages"][command] = {
-        #     "name": "core",
-        #     "version": "1.0.0",
-        #     "license": "ISC",
-        #     "path": "/aqua_modules/auth/initialise.php"
-        # }
-        file_contents["packages"][command] = data["config-lock"][command]
-        file_contents = json.dumps(file_contents, indent=4)
-        # print(file_contents)
-        # Print the contents
+        with open('config.json', 'r') as file:
+            # Read the entire contents of the file
+            file_contents = file.read()
+            file_contents = json.loads(file_contents)
+            # file_contents["dependencies"][command] = {
+            #     "enable": False,
+            #     "path": "auth"
+            # }
+            file_contents["dependencies"][command] = data["config"][command]
+            file_contents = json.dumps(file_contents, indent=4)
+            # print(file_contents)
+            # Print the contents
 
-    with open('config-lock.json', 'w') as f:
-        f.write(file_contents)
+        with open('config.json', 'w') as f:
+            f.write(file_contents)
+        
+        with open('config-lock.json', 'r') as file:
+            # Read the entire contents of the file
+            file_contents = file.read()
+            file_contents = json.loads(file_contents)
+            # file_contents["packages"][command] = {
+            #     "name": "core",
+            #     "version": "1.0.0",
+            #     "license": "ISC",
+            #     "path": "/aqua_modules/auth/initialise.php"
+            # }
+            file_contents["packages"][command] = data["config-lock"][command]
+            file_contents = json.dumps(file_contents, indent=4)
+            # print(file_contents)
+            # Print the contents
 
-    print(color_green+"\npackage installed")
+        with open('config-lock.json', 'w') as f:
+            f.write(file_contents)
+
+        print(color_green+"Package Installed")
+    else:
+        print(color_red+"Package Install Failed")
 
 def install(command):
     print(color_blue+"preparing to install... \n")
@@ -122,7 +152,20 @@ def install(command):
         # print('Error:', response.status_code)
         print(color_red+data["message"])
 
-    
+def uninstall(command):
+    commands = command.split()
+    path_to_remove = 'aqua_modules/package/'+commands[1]
+    # Check if the path exists
+    if os.path.exists(path_to_remove):
+        try:
+            # Attempt to remove the directory
+            shutil.rmtree(path_to_remove)
+            print(f"Successfully removed: {path_to_remove}")
+        except Exception as e:
+            # Handle any exceptions that occur
+            print(f"Error removing {path_to_remove}: {e}")
+    else:
+        print(f"Path does not exist: {path_to_remove}")
 
 #     _                     _____  _   _ _____
 #    / \   __ _ _   _     _|  __ \| | | |  __ \
@@ -152,7 +195,7 @@ while True:
     elif command.startswith("install"):
         install(command)
     elif command.startswith("uninstall"):
-        print("uninstall started")
+        uninstall(command)
     elif command == "exit":
         break
     else:
